2fd830fb4bacff03a20e093947655f5b0265ed85
unknownwyj <wongyijin11@gmail.com>
2022-12-17 21:14:58 +0800

    update to sw

--- sw.js
+++ sw.js
@@ -11,8 +11,34 @@ self.addEventListener("message", (event) => {
 });
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
+  new RegExp('*'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: CACHE
   })
 );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(async function() {
+     try{
+       var res = await fetch(event.request);
+       var cache = await caches.open('cache');
+       cache.put(event.request.url, res.clone());
+       return res;
+     }
+     catch(error){
+       return caches.match(event.request);
+      }
+    }());
+});
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('cache').then(function(cache) {
+      return cache.addAll([
+        "*",
+        "./app/index.html",
+        "./app/style.css",
+        "./app/app.js"
+       ]);
+    })
+   );
+});
